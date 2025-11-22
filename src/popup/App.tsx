47473a778491b2de15180d3ec4c3d1021611b954
kocya <sample@test.co.jp>
2025-11-23 00:20:54 +0900

    initial commit

---
+++ src/popup/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ width: "300px", padding: "10px" }}>
+      <h1>GitHub Notify</h1>
+      <p>通知はありません</p>
+    </div>
+  );
+};
+
+export default App;
